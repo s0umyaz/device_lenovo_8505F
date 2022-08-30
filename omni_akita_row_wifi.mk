eaa7991a155753c57758cfde3cd5a76692fe8148
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-30 18:41:29 +0200

    akita_row_wifi: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: lenovo Model: Lenovo TB-8505F Script version: 2.4.0

---
+++ omni_akita_row_wifi.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from akita_row_wifi device
+$(call inherit-product, device/lenovo/akita_row_wifi/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := akita_row_wifi
+PRODUCT_NAME := omni_akita_row_wifi
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo TB-8505F
+PRODUCT_MANUFACTURER := lenovo
