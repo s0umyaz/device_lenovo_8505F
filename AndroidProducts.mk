22707ba7210499280ac51140d758b0143278ce86
Yasuda Minori <80877391+s0umyaz@users.noreply.github.com>
2022-09-01 08:07:41 -0400

    updated common-lunch-choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,4 @@
-#
+# Copyright (C) 2022 s0umyaz aka Soumyadeep Ghosh
 # Copyright (C) 2022 The Android Open Source Project
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
@@ -9,6 +9,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_akita_row_wifi.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_akita_row_wifi-user \
-    omni_akita_row_wifi-userdebug \
     omni_akita_row_wifi-eng
