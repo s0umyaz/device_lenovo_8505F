805ded69cfc497770e3498a8291fa3b4d4d5ad21
Yasuda Minori <80877391+s0umyaz@users.noreply.github.com>
2022-09-01 08:45:25 -0400

    if-exists

--- omni_akita_row_wifi.mk
+++ omni_akita_row_wifi.mk
@@ -7,15 +7,15 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from akita_row_wifi device
 $(call inherit-product, device/lenovo/akita_row_wifi/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product-if-exists, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := akita_row_wifi
