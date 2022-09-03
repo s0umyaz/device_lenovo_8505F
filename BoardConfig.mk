d17eb11a0481ce1dec93d27b052c2128bbb3f64a
Carlo Dee <109793453+carlodeeCrypton@users.noreply.github.com>
2022-09-03 19:20:39 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,7 +49,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 buildvariant=user
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-#TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb/mt6761.dtb
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb/mt6761.dtb
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 TARGET_RECOVERY_INITRC := $(DEVICE_PATH)/recovery/root/init.recovery.mt6761.rc
 TARGET_KERNEL_SOURCE := kernel/lenovo/akita_row_wifi
